456c03f23194faa36b6677d337126b5db2991eb4
JohnLevander <jdl50@pitt.edu>
2017-11-08 16:58:45 -0500

    still trying to fix windows valdiation

--- dats/dats_model.py
+++ dats/dats_model.py
@@ -16,9 +16,9 @@ def validate_dataset(path, filename, error_printing):
     try:
         dataset_schema_file = open(join(DATS_schemasPath,"dataset_schema.json"))
         datasetSchema = json.load(dataset_schema_file)
-        url = 'file://'+DATS_schemasPath + "/" + "dataset_schema.json"
-        url = url.replace("\\", "/")
-        resolver = RefResolver(url, datasetSchema) #, base_uri=schemasPath)
+        url = DATS_schemasPath + "/" + "dataset_schema.json"
+        url = url.replace("/", "\\\\")
+        resolver = RefResolver('file://' + url, datasetSchema) #, base_uri=schemasPath)
         validator = Draft4Validator(datasetSchema, resolver=resolver)
         logger.info("Validating %s", filename)
 
